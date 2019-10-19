7c215f269315c68b5741da630723d773d1087e9c
charcambau <45144343+charcambau@users.noreply.github.com>
2019-10-19 16:36:43 +0200

    fix fix fix

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ closeAbout.addEventListener('click', closeTheAbout);
 function showAbout () {
   document.getElementById('aboutSection').style.transform = 'translateX(0)';
   document.getElementById('aboutSection').style.display = "block";
+  document.getElementById('aboutSection').style.opacity = "1";
 }
 
 function closeTheAbout () {
   document.getElementById('aboutSection').style.transform = 'translateX(50%)';
   document.getElementById('aboutSection').style.display = 'none';
+  document.getElementById('aboutSection').style.opacity = "0";
 }
