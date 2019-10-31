a2b5abc1c77e07d1bec5156ae64d7b284ad3729a
Charlotte <charlottecambau@gmail.com>
2019-10-31 15:10:18 +0100

    Started trying to create a separate box for the brief--

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var about = document.getElementById('about');
 var closeAbout = document.getElementById('crossAbout');
+var moreBrief = document.getElementById('moreBrief');
 
 about.addEventListener('click', showAbout);
 closeAbout.addEventListener('click', closeTheAbout);
+moreBrief.addEventListener('click', showBrief);
 
 function showAbout () {
   document.getElementById('aboutSection').style.transform = 'translateX(0)';
@@ -14,4 +16,8 @@ function closeTheAbout () {
   document.getElementById('aboutSection').style.transform = 'translateX(20%)';
   document.getElementById('aboutSection').style.display = 'none';
   document.getElementById('aboutSection').style.opacity = "0";
+}
+
+function showBrief () {
+  document.getElementById('briefFork').style.display = 'block';
 }
