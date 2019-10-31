1866816e6c9de09e6287664a8310a5c4a902f940
Charlotte <charlottecambau@gmail.com>
2019-10-31 16:19:35 +0100

    js & css(brief-the fork) tried to work on the project page-- a finir

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var about = document.getElementById('about');
 var closeAbout = document.getElementById('crossAbout');
 var moreBrief = document.getElementById('moreBrief');
+var lessBrief = document.getElementById('lessBrief');
 
 about.addEventListener('click', showAbout);
 closeAbout.addEventListener('click', closeTheAbout);
 moreBrief.addEventListener('click', showBrief);
+lessBrief.addEventListener('click', hideBrief);
 
 function showAbout () {
   document.getElementById('aboutSection').style.transform = 'translateX(0)';
@@ -15,9 +17,17 @@ function showAbout () {
 function closeTheAbout () {
   document.getElementById('aboutSection').style.transform = 'translateX(20%)';
   document.getElementById('aboutSection').style.display = 'none';
-  document.getElementById('aboutSection').style.opacity = "0";
+  document.getElementById('aboutSection').style.opacity = "1";
 }
 
 function showBrief () {
   document.getElementById('briefFork').style.display = 'block';
+  document.getElementById('moreBrief').style.display = 'none';
+  document.getElementById('lessBrief').style.display = 'block';
+}
+
+function hideBrief () {
+  document.getElementById('briefFork').style.display = 'none';
+  document.getElementById('moreBrief').style.display = 'block';
+  document.getElementById('lessBrief').style.display = 'none';
 }
